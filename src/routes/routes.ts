2918443f43211df1f8ed0e4bb41b2961f38da2af
VitorKaeZ <vitorcaetanoz@outlook.com>
2022-05-12 21:28:21 -0300

    3 day

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,7 +5,26 @@ const router = Router()
 
 //GET , POST , PUT, DELETE
 router.get('/', (req: Request, res: Response)=>{
-    res.render('home')
+    let user: { name: string; age: number }= {
+        name:'Vitor',
+        age: 51
+    }
+    let showWelcome: boolean = false
+
+    if (user.age > 50) {
+        showWelcome = true
+    }
+
+
+    res.render('home', {
+        user,
+        showWelcome,
+        products: [
+            {title: 'Produto 1', price: 10},
+            {title: 'Produto 2', price: 15},
+            {title: 'Produto 3', price: 20}
+        ]
+    })
 })
 router.get('/contato', (req: Request, res: Response)=>{
     res.send('Formulario de Contato')
